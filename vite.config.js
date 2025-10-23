11dd58172b4b23bfa3c4c206469ef5432edd5232
Sajith Bududdhika <sajithbududdhika@gmail.com>
2025-10-23 11:36:06 +0530

    Add favicon

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// D:\zuno\frontend\vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/zuno1/',
-  build: { outDir: 'docs' }, // build straight into /docs
-});
+  base: '/zuno1/',        // project path
+  build: { outDir: 'docs' } // build straight into /docs
+})
